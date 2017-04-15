9367b8cb3dbdcafcbfbae4d62cdc57d009931b0c
Eraldo Energy <eraldo@eraldo.org>
2017-04-15 20:44:06 +0200

    Added home and feedback pages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,40 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
+export class App {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = 'HomePage';
+  activePage: any;
 
-  pages: Array<{title: string, component: any}>;
+  apps: Array<{name: string, component: any, icon?: string, color?: string}>;
+  pages: Array<{name: string, component: any, icon?: string, color?: string}>;
+  feedbackPage: {name: string, component: any, icon?: string, color?: string};
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+    this.apps = [
+      { name: 'Home', component: 'HomePage', icon: 'home', color: 'area-1'},
+      { name: 'Manager', component: 'ManagerPage', icon: 'briefcase', color: 'area-3' },
+      { name: 'Journal', component: 'JournalPage', icon: 'book', color: 'area-5' },
+      { name: 'Community', component: 'CommunityPage', icon: 'people', color: 'area-4' },
+      { name: 'Journey', component: 'JourneyPage', icon: 'plane', color: 'area-2' },
+      { name: 'Academy', component: 'AcademyPage', icon: 'school', color: 'area-6' },
+      { name: 'Vision', component: 'VisionPage', icon: 'eye', color: 'area-7' },
     ];
+    this.pages = [
+      { name: 'Support', component: 'SupportPage', icon: 'help-circle', color: 'mid' },
+      { name: 'Settings', component: 'SettingsPage', icon: 'settings', color: 'mid' },
+      { name: 'About', component: 'AboutPage', icon: 'information-circle', color: 'mid' },
+      { name: 'Contact', component: 'ContactPage', icon: 'mail', color: 'mid' },
+      { name: 'Lab', component: 'LabPage', icon: 'flask', color: 'light' },
+    ];
+    this.feedbackPage = { name: 'Feedback', component: 'FeedbackPage', icon: 'paper-plane', color: 'mid' };
+    this.activePage = this.apps[0]
 
   }
 
@@ -31,8 +44,10 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if (this.platform.is('cordova')) {
+        this.statusBar.styleDefault();
+        this.splashScreen.hide();
+      }
     });
   }
 
@@ -40,5 +55,15 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+    this.activePage = page;
   }
+
+  pushPage(page) {
+    this.nav.push(page.component);
+  }
+
+  checkActive(page) {
+    return page == this.activePage;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { App } from './app.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
-    ListPage
+    App,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(App),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
+    App,
   ],
   providers: [
     StatusBar,
