8295661cb4ccc69ef5572d50c6dcf892ec930a4e
Eraldo Energy <eraldo@eraldo.org>
2017-04-22 21:59:00 +0200

    Added profile mockup.

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,17 +2,20 @@ export interface UserObject {
   id: string,
   name: string
   email?: string,
+  image?: string,
 }
 
 export class User implements UserObject {
   public id: string;
   public name: string;
   public email?: string;
+  public image?: string;
 
   constructor(userObject: UserObject) {
     this.id = userObject.id;
     this.name = userObject.name;
     this.email = userObject.email;
+    this.image = userObject.image;
   }
 
   get authenticated() {
@@ -24,6 +27,8 @@ export class User implements UserObject {
   }
 }
 
-const ANONYMOUS_USER_OBJECT: UserObject = {id: undefined, name: 'Anonymous'};
+const DEFAULT_USER_IMAGE = 'http://santetotal.com/wp-content/uploads/2014/05/default-user.png';
+
+const ANONYMOUS_USER_OBJECT: UserObject = {id: undefined, name: 'Anonymous', image: DEFAULT_USER_IMAGE};
 
 export const ANONYMOUS_USER: User = User.fromObject(ANONYMOUS_USER_OBJECT);
--- src/pages/community/profile/profile.ts
+++ src/pages/community/profile/profile.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, OnInit} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {Observable} from "rxjs/Observable";
+import {ANONYMOUS_USER, User} from "../../../models/user";
+import {UserService} from "../../../services/user/user";
 
-/**
- * Generated class for the ProfilePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
-export class ProfilePage {
+export class ProfilePage implements OnInit {
+  user$: Observable<User>;
+  default_image = ANONYMOUS_USER.image;
+  // default_hero_image = ANONYMOUS_USER.image;
+  // default_demon_image = ANONYMOUS_USER.image;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.user$ = this.userService.user$;
   }
 
   ionViewDidLoad() {
