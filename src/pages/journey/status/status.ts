940df80bcb4676988cd326cf3e580277a6d05c0f
Eraldo Energy <eraldo@eraldo.org>
2017-05-17 02:05:38 +0200

    Added courses mockup and moved some files.

--- src/pages/journey/status/status.ts
+++ src/pages/journey/status/status.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-const components = ['chapter', 'stats', 'inventory'];
-
 @IonicPage()
 @Component({
   selector: 'page-status',
   templateUrl: 'status.html',
 })
 export class StatusPage {
-  components = components;
-  selectedComponent = components[0];
+  progress: number = 60;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
