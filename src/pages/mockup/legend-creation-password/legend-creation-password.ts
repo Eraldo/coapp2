0c8e6f8a6ae799b2cdd8d3c8fc1b7baa5f86b2a9
Eraldo Energy <eraldo@eraldo.org>
2017-06-01 04:19:53 +0200

    Added password confirmation feature v1.

--- src/pages/mockup/legend-creation-password/legend-creation-password.ts
+++ src/pages/mockup/legend-creation-password/legend-creation-password.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the LegendCreationPasswordPage page.
@@ -13,8 +13,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'legend-creation-password.html',
 })
 export class LegendCreationPasswordPage {
+  @ViewChild('passwordInput') passwordInput;
+  password: '';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  reset() {
+    this.password = ''
+  }
+
+  submit(password) {
+    console.log(password);
+    if (!!this.password && this.password == password) {
+      this.next();
+    } else {
+      this.passwordInput.value = '';
+      this.password = password;
+    }
+  }
+
+  next() {
+    this.navCtrl.push('PrologueMockupPage')
   }
 
   ionViewDidLoad() {
