fb5a178427dbfb7c0f03c0ab32b8443f933cde1a
Eraldo Energy <eraldo@eraldo.org>
2017-06-01 16:53:24 +0200

    Updated welcome page.

--- src/pages/mockup/welcome/welcome.ts
+++ src/pages/mockup/welcome/welcome.ts
@@ -1,20 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
-/**
- * Generated class for the WelcomePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-welcome',
   templateUrl: 'welcome.html',
 })
-export class WelcomePage {
+export class WelcomePage implements OnInit {
+  enterForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.enterForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  enterWithEmail() {
+    if (this.enterForm.valid) {
+      const email = this.enterForm.value.email;
+      // Checking if the user is known.
+      if (email == 'eraldo@eraldo.org') {
+        // User has account.
+        this.navCtrl.push('LegendCreationPasswordPage')
+      } else {
+        // User is new.
+        this.navCtrl.push('LegendCreationNamePage')
+      }
+      // const email = this.enterForm.value.email;
+      // const password = this.enterForm.value.password;
+      // this.userService.join(email, password)
+      // // .then(() => this.success())
+      //   .catch(error => alert(error.message));
+    } else {
+      alert('TODO: Implementing logic when form not valid');
+    }
+  }
+
+  enterWithGoogle() {
+    alert('TODO: Use google to enter');
+  }
+
+  enterWithFacebook() {
+    alert('TODO: Use facebook to enter');
   }
 
   ionViewDidLoad() {
