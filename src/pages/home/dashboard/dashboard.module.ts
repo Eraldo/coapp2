299659bb09fc95cd94687f64261bdfb8a1ffa7de
Eraldo Energy <eraldo@eraldo.org>
2017-06-02 17:46:48 +0200

    Adding a progress bar.

--- src/pages/home/dashboard/dashboard.module.ts
+++ src/pages/home/dashboard/dashboard.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { DashboardPage } from './dashboard';
+import {ProgressBarComponent} from "../../../components/progress-bar/progress-bar";
 
 @NgModule({
   declarations: [
     DashboardPage,
+    ProgressBarComponent
   ],
   imports: [
     IonicPageModule.forChild(DashboardPage),
