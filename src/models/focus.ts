4ff4ed69e23993763da9a1a7defba95a668375da
Eraldo Energy <eraldo@eraldo.org>
2017-06-23 11:10:37 +0200

    Added focus model.

---
+++ src/models/focus.ts
@@ -0,0 +1,31 @@
+import {Scope} from "./scope";
+
+export interface FocusObject {
+  id: string;
+  owner: string;
+  scope: Scope;
+  start: Date;
+  outcome1: string;
+  outcome2: string;
+  outcome3: string;
+  outcome4: string;
+  createdAt?: Date;
+  modifiedAt?: Date;
+}
+
+export class Focus implements FocusObject {
+  id: string;
+  owner: string;
+  scope: Scope;
+  start: Date;
+  outcome1: string;
+  outcome2: string;
+  outcome3: string;
+  outcome4: string;
+  createdAt?: Date;
+  modifiedAt?: Date;
+
+  constructor(focusObject: FocusObject) {
+    Object.assign(this, focusObject);
+  }
+}
