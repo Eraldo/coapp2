a9d94acbd2b29d0e278e1cab1fbfdfbd4bf64791
Eraldo Energy <eraldo@eraldo.org>
2017-06-24 18:13:50 +0200

    Moved hero and demon to journey.

--- src/pages/arcade/adventures/adventures.module.ts
+++ src/pages/arcade/adventures/adventures.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AdventuresPage } from './adventures';
+import {ProgressBarComponentModule} from "../../../components/progress-bar/progress-bar.module";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { AdventuresPage } from './adventures';
   ],
   imports: [
     IonicPageModule.forChild(AdventuresPage),
+    ProgressBarComponentModule,
   ],
   exports: [
     AdventuresPage
