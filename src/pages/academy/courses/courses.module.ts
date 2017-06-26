8cd2310fcecef43e21a218fe6851f8f6a2759e2c
Eraldo Energy <eraldo@eraldo.org>
2017-06-26 03:01:55 +0200

    Added round progress bars to courses page.

--- src/pages/academy/courses/courses.module.ts
+++ src/pages/academy/courses/courses.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CoursesPage } from './courses';
 import {AppToolbarComponentModule} from "../../../components/app-toolbar/app-toolbar.module";
+import {RoundProgressModule} from "angular-svg-round-progressbar";
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import {AppToolbarComponentModule} from "../../../components/app-toolbar/app-too
   imports: [
     IonicPageModule.forChild(CoursesPage),
     AppToolbarComponentModule,
+    RoundProgressModule,
   ],
   exports: [
     CoursesPage
