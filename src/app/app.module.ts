4d2b8a3317c14ace5256073cf3491e73ab56e837
Eraldo Energy <eraldo@eraldo.org>
2017-07-27 18:36:39 +0200

    Cleanup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {ErrorHandler, NgModule} from '@angular/core';
-import {IonicApp, IonicErrorHandler, IonicModule, NavController} from 'ionic-angular';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 
 import {App} from './app.component';
 
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {UserService} from "../services/user/user";
-// import {AngularFireModule} from "angularfire2";
-// import {firebaseConfig} from "../environments/firebase.config";
-
 import 'rxjs/Rx';
 import {MomentModule} from "angular2-moment";
 import {ScopeService} from "../services/scope/scope";
 import {OutcomeService} from "../services/outcome/outcome";
-// import {AngularFireAuthModule} from "angularfire2/auth";
-// import {AngularFireDatabaseModule} from "angularfire2/database";
 import {LocationService} from "../services/location/location";
-import {Http, HttpModule} from "@angular/http";
+import {HttpModule} from "@angular/http";
 import {IonicStorageModule} from "@ionic/storage";
 import {GooglePlus} from "@ionic-native/google-plus";
 import {FocusService} from "../services/focus/focus";
