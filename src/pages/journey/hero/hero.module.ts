28a9ff82e297267a3d43dc794cd5e4f5c92ba98a
Eraldo Energy <eraldo@eraldo.org>
2017-09-11 11:26:38 +0200

    Added dynamic hero component.

--- src/pages/journey/hero/hero.module.ts
+++ src/pages/journey/hero/hero.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HeroPage } from './hero';
 import {AppToolbarComponentModule} from "../../../components/app-toolbar/app-toolbar.module";
+import {MarkdownModule} from "angular2-markdown";
+import {ElasticModule} from "angular2-elastic";
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import {AppToolbarComponentModule} from "../../../components/app-toolbar/app-too
   imports: [
     IonicPageModule.forChild(HeroPage),
     AppToolbarComponentModule,
+    MarkdownModule,
+    ElasticModule
   ],
   exports: [
     HeroPage
--- src/pages/journey/hero/hero.ts
+++ src/pages/journey/hero/hero.ts
@@ -1,5 +1,42 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import gql from "graphql-tag";
+import {Apollo} from "apollo-angular";
+import {MarkdownService} from "angular2-markdown";
+import {Observable} from "rxjs/Observable";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+const MyUserHero = gql`
+  query {
+    myUser {
+      hero {
+        id
+        content
+      }
+    }
+  }
+`;
+
+const updateHero = gql`
+  mutation updateHero($content: String!) {
+    updateHero(input: {content: $content}) {
+      hero {
+        id
+        content
+      }
+    }
+  }
+`;
+
+
+interface QueryResponse {
+  myUser: {
+    hero: {
+      content: string
+    }
+  }
+  loading
+}
 
 @IonicPage()
 @Component({
@@ -7,8 +44,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'hero.html',
 })
 export class HeroPage {
+  data$: Observable<QueryResponse>;
+  loading$: Observable<boolean>;
+  content$: Observable<string>;
+  editing = false;
+  private form: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo, private formBuilder: FormBuilder, private markdownService: MarkdownService) {
+    // Workaround: https://github.com/dimpu/angular2-markdown/issues/65
+    // this.markdownService.setMarkedOptions({gfm: true, breaks: true, sanitize: true});
+    this.markdownService.setMarkedOptions({gfm: true, breaks: true});
+
+    this.form = this.formBuilder.group({
+      content: ['', Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.data$ = this.apollo.watchQuery<QueryResponse>({query: MyUserHero, fetchPolicy: "cache-and-network"})
+      .map(({data}) => data);
+    this.loading$ = this.data$.map(data => data.loading);
+    this.content$ = this.data$.map(data => data.myUser.hero.content);
+    this.content$.subscribe(content => this.form.setValue({content}))
+  }
+
+  edit() {
+    this.editing = true;
+  }
+
+  save() {
+    const content = this.form.value.content;
+    console.log(content);
+    this.editing = false;
+
+    this.apollo.mutate({
+      mutation: updateHero,
+      variables: {
+        content: content
+      }
+    }).subscribe(({ data }) => {
+      console.log('got data', data);
+    },(error) => {
+      console.log('there was an error sending the query', error);
+    });
   }
 
   ionViewDidLoad() {
