c42c71b499f4f034b449c2da8f1015cd9a9923b9
Eraldo Energy <eraldo@eraldo.org>
2017-09-14 00:48:32 +0200

    Refactored legend page to use graphql api.

--- src/pages/community/legend/legend.ts
+++ src/pages/community/legend/legend.ts
@@ -1,8 +1,93 @@
 import {Component} from '@angular/core';
 import {AlertController, IonicPage, NavController, NavParams, PopoverController} from 'ionic-angular';
+import {ANONYMOUS_USER} from "../../../models/user";
+import {Apollo} from "apollo-angular";
+import gql from "graphql-tag";
 import {Observable} from "rxjs/Observable";
-import {ANONYMOUS_USER, User} from "../../../models/user";
-import {UserService} from "../../../services/user/user";
+
+const MyUserQuery = gql`
+  query {
+    user: myUser {
+      id
+    }
+  }
+`;
+
+const UserQuery = gql`
+  query User($id: ID!) {
+    user(id: $id) {
+      id
+      username
+      name
+      avatar
+      gender
+      purpose
+      dateJoined
+      level
+    }
+  }
+`;
+
+const UpdateUsernameMutation = gql`
+  mutation UpdateUsername($username: String!) {
+    updateUser(input: {username: $username}) {
+      user {
+        id
+        username
+      }
+    }
+  }
+`;
+
+const UpdateNameMutation = gql`
+  mutation UpdateName($name: String!) {
+    updateUser(input: {name: $name}) {
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
+
+const UpdateGenderMutation = gql`
+  mutation UpdateGender($gender: String!) {
+    updateUser(input: {gender: $gender}) {
+      user {
+        id
+        gender
+      }
+    }
+  }
+`;
+
+const UpdatePurposeMutation = gql`
+  mutation UpdatePurpose($purpose: String!) {
+    updateUser(input: {purpose: $purpose}) {
+      user {
+        id
+        purpose
+      }
+    }
+  }
+`;
+
+interface User {
+  id
+  username
+  name
+  avatar
+  gender
+  purpose
+  dateJoined
+  level
+}
+
+interface UserResponse {
+  user: User
+  loading
+}
+
 
 @IonicPage()
 @Component({
@@ -14,17 +99,18 @@ export class LegendPage {
   currentUser$: Observable<User>;
   default_image = ANONYMOUS_USER.image;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public userService: UserService, public popoverCtrl: PopoverController, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController, public alertCtrl: AlertController, private apollo: Apollo) {
   }
 
   ngOnInit(): void {
-    this.currentUser$ = this.userService.user$;
-    const id = this.navParams.get('id');
-    if (id) {
-      this.user$ = this.userService.getUserById$(id);
-    } else {
-      this.user$ = this.currentUser$;
-    }
+    this.currentUser$ = this.apollo.watchQuery<UserResponse>({
+      query: MyUserQuery
+    }).map(({data}) => data.user);
+    const id$ = this.currentUser$.map(user => this.navParams.get('id') || user.id);
+    this.user$ = this.apollo.watchQuery<UserResponse>({
+      query: UserQuery,
+      variables: {id: id$}
+    }).map(({data}) => data.user);
   }
 
   showOptions(source) {
@@ -41,7 +127,7 @@ export class LegendPage {
             {
               name: 'name',
               placeholder: 'Name',
-              value: user.name
+              value: legend.name
             },
           ],
           buttons: [
@@ -56,7 +142,13 @@ export class LegendPage {
               handler: data => {
                 const name = data.name;
                 if (name && name.length >= 4) {
-                  this.userService.updateUser({name});
+
+                  this.apollo.mutate({
+                    mutation: UpdateNameMutation,
+                    variables: {
+                      name: name
+                    }
+                  });
                 } else {
                   // TODO: Show error message: "Name has to be at least 4 characters long."
                 }
@@ -78,7 +170,7 @@ export class LegendPage {
             {
               name: 'username',
               placeholder: 'Username',
-              value: user.username
+              value: legend.username
             },
           ],
           buttons: [
@@ -93,7 +185,12 @@ export class LegendPage {
               handler: data => {
                 const username = data.username;
                 if (username && username.length >= 4) {
-                  this.userService.updateUser({username});
+                  this.apollo.mutate({
+                    mutation: UpdateUsernameMutation,
+                    variables: {
+                      username: username
+                    }
+                  });
                 } else {
                   // TODO: Show error message: "Username has to be at least 4 characters long."
                 }
@@ -115,7 +212,7 @@ export class LegendPage {
             {
               name: 'purpose',
               placeholder: 'Purpose',
-              value: user.purpose
+              value: legend.purpose
             },
           ],
           buttons: [
@@ -130,7 +227,14 @@ export class LegendPage {
               handler: data => {
                 const purpose = data.purpose;
                 if (purpose && purpose.length >= 4) {
-                  this.userService.updateUser({purpose});
+                  // this.userService.updateUser({purpose});
+                  this.apollo.mutate({
+                    mutation: UpdatePurposeMutation,
+                    variables: {
+                      purpose: purpose
+                    }
+                  });
+
                 } else {
                   // TODO: Show error message: "Purpose has to be at least 4 characters long."
                 }
@@ -153,19 +257,19 @@ export class LegendPage {
               type: 'radio',
               label: 'Male',
               value: 'M',
-              checked: user.gender == 'M'
+              checked: legend.gender == 'M'
             },
             {
               type: 'radio',
               label: 'Female',
               value: 'F',
-              checked: user.gender == 'F'
+              checked: legend.gender == 'F'
             },
             {
               type: 'radio',
               label: 'Neutral',
               value: 'N',
-              checked: user.gender == 'N'
+              checked: legend.gender == 'N'
             },
           ],
           buttons: [
@@ -179,7 +283,12 @@ export class LegendPage {
               text: 'Save',
               handler: data => {
                 const gender = data;
-                  this.userService.updateUser({gender});
+                this.apollo.mutate({
+                  mutation: UpdateGenderMutation,
+                  variables: {
+                    gender: gender
+                  }
+                });
               }
             }
           ]
