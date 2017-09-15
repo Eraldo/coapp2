5cdbf254590329b5cf554cd5f5c26d46c9fc4e71
Eraldo Energy <eraldo@eraldo.org>
2017-09-16 01:57:55 +0200

    Added dynamic dashboard action card.

--- src/pages/home/dashboard/dashboard.ts
+++ src/pages/home/dashboard/dashboard.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import gql from "graphql-tag";
+import {Apollo} from "apollo-angular";
+
+const SuggestedActionQuery = gql`
+  query {
+    suggestedAction
+  }
+`;
 
 @IonicPage()
 @Component({
@@ -7,8 +15,23 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
   templateUrl: 'dashboard.html',
 })
 export class DashboardPage {
+  query$;
+  loading = true;
+  action: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo) {
+  }
+
+  ngOnInit() {
+    this.query$ = this.apollo.watchQuery({query: SuggestedActionQuery})
+    this.query$.subscribe(({data, loading}) => {
+      this.loading = loading;
+      this.action = data && data.suggestedAction;
+    })
+  }
+
+  ionViewDidEnter() {
+    this.query$.refetch()
   }
 
   ionViewDidLoad() {
