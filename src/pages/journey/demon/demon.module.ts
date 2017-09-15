b2c02e7148d95a1bfb657a1ffefb2f06d157e27f
Eraldo Energy <eraldo@eraldo.org>
2017-09-16 01:31:37 +0200

    Added demon content.

--- src/pages/journey/demon/demon.module.ts
+++ src/pages/journey/demon/demon.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { DemonPage } from './demon';
 import {AppToolbarComponentModule} from "../../../components/app-toolbar/app-toolbar.module";
+import {MomentModule} from "angular2-moment";
+import {MarkdownModule} from "angular2-markdown";
+import {SimplemdeModule} from "ng2-simplemde/no-style";
 
 @NgModule({
   declarations: [
@@ -10,6 +13,9 @@ import {AppToolbarComponentModule} from "../../../components/app-toolbar/app-too
   imports: [
     IonicPageModule.forChild(DemonPage),
     AppToolbarComponentModule,
+    MomentModule,
+    MarkdownModule,
+    SimplemdeModule,
   ],
   exports: [
     DemonPage
--- src/pages/journey/demon/demon.ts
+++ src/pages/journey/demon/demon.ts
@@ -1,20 +1,117 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import gql from "graphql-tag";
+import {Apollo} from "apollo-angular";
+import {MarkdownService} from "angular2-markdown";
+import {Observable} from "rxjs/Observable";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
-/**
- * Generated class for the DemonPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+const MyUserDemon = gql`
+  query {
+    myUser {
+      id
+      demon {
+        id
+        content
+        modified
+      }
+    }
+  }
+`;
+
+const updateDemon = gql`
+  mutation updateDemon($content: String!) {
+    updateDemon(input: {content: $content}) {
+      demon {
+        id
+        content
+        modified
+      }
+    }
+  }
+`;
+
+
+interface QueryResponse {
+  myUser: {
+    demon: {
+      content: string,
+      modified: string
+    }
+  }
+  loading
+}
+
 @IonicPage()
 @Component({
   selector: 'page-demon',
   templateUrl: 'demon.html',
 })
 export class DemonPage {
+  query$;
+  loading$: Observable<boolean>;
+  content$: Observable<string>;
+  lastUpdated$: Observable<string>;
+  editing = false;
+  form: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo, private formBuilder: FormBuilder, private markdownService: MarkdownService) {
+    // Workaround: https://github.com/dimpu/angular2-markdown/issues/65
+    // this.markdownService.setMarkedOptions({gfm: true, breaks: true, sanitize: true});
+    this.markdownService.setMarkedOptions({gfm: true, breaks: true});
+
+    this.form = this.formBuilder.group({
+      content: ['', Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.query$ = this.apollo.watchQuery<QueryResponse>({query: MyUserDemon});
+    this.loading$ = this.query$.map(({data}) => data.loading);
+    this.lastUpdated$ = this.query$.map(({data}) => data.myUser.demon.modified);
+    this.content$ = this.query$.map(({data}) => {
+      if (data) {
+        return data.myUser.demon.content
+      } else {
+        return ''
+      }
+    });
+  }
+
+  ionViewDidEnter() {
+    this.query$.refetch()
+  }
+
+  edit() {
+    this.editing = true;
+  }
+
+  update(content) {
+    this.form.setValue({content});
+  }
+
+  save() {
+    const content = this.form.value.content;
+    this.editing = false;
+
+    this.content$.first().subscribe(originalContent => {
+      if (content && content != originalContent) {
+        this.updateDemon(content)
+      }
+    });
+  }
+
+  updateDemon(content) {
+    this.apollo.mutate({
+      mutation: updateDemon,
+      variables: {
+        content: content
+      }
+    }).subscribe(({data}) => {
+      console.log('got data', data);
+    }, (error) => {
+      console.log('there was an error sending the query', error);
+    });
   }
 
   ionViewDidLoad() {
