cd4716512f3795a83c6d9776899a85a95123b3da
Eraldo Energy <eraldo@eraldo.org>
2017-09-17 17:36:33 +0200

    Refactored interview to use graphql backend.

--- src/pages/studio/interview/interview.ts
+++ src/pages/studio/interview/interview.ts
@@ -3,10 +3,47 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {ScopeService} from "../../../services/scope/scope";
 import {InterviewEntry} from "../../../models/interview";
 import {Observable} from "rxjs/Observable";
-import {InterviewService} from "../../../services/interview/interview";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {DateService} from "../../../services/date/date";
-import {Scope} from "../../../models/scope";
+import {getScopeStart, Scope} from "../../../models/scope";
+import {Apollo} from "apollo-angular";
+import gql from "graphql-tag";
+
+const InterviewEntryQuery = gql`
+  query InterviewEntry($scope: String!, $start: String!) {
+    myUser {
+      id
+      interviewEntries(scope: $scope, start: $start) {
+        edges {
+          node {
+            id
+            scope
+            start
+            likes
+            dislikes
+          }
+        }
+      }
+    }
+  }
+`;
+
+const AddInterviewEntryMutation = gql`
+  mutation AddInterviewEntry($scope: Scope!, $start: DateTime!, $likes: String!, $dislikes: String!) {
+    addInterviewEntry(input: {scope: $scope, start: $start, likes: $likes, dislikes: $dislikes}) {
+      interviewEntry {
+        id
+        owner {
+          id
+        }
+        scope
+        start
+        likes
+        dislikes
+      }
+    }
+  }
+`;
 
 @IonicPage()
 @Component({
@@ -14,21 +51,46 @@ import {Scope} from "../../../models/scope";
   templateUrl: 'interview.html',
 })
 export class InterviewPage {
+  query$;
+  loading = true;
   entry$: Observable<InterviewEntry>;
   scope$: Observable<Scope>;
   private form: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private scopeService: ScopeService, private dateService: DateService, private interviewService: InterviewService, private formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private scopeService: ScopeService, private dateService: DateService, private apollo: Apollo, private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
-    this.entry$ = this.interviewService.entry$;
+    // this.entry$ = this.interviewService.entry$;
     this.scope$ = this.scopeService.scope$;
 
     this.form = this.formBuilder.group({
       likes: ['', Validators.required],
       dislikes: ['', Validators.required],
     });
+
+    Observable.combineLatest(
+      this.scopeService.scope$,
+      this.dateService.date$,
+      (scope, date) => {
+        this.query$ = this.apollo.watchQuery({
+          query: InterviewEntryQuery,
+          variables: {
+            scope: scope,
+            start: getScopeStart(scope, date)
+          }
+        });
+        this.entry$ = this.query$.map(({data, loading}) => {
+          this.loading = loading;
+          return data &&
+            data.myUser.interviewEntries.edges[0] &&
+            data.myUser.interviewEntries.edges[0].node;
+        });
+      }).subscribe();
+  }
+
+  ionViewDidEnter() {
+    this.query$.refetch()
   }
 
   selectScope() {
@@ -38,13 +100,25 @@ export class InterviewPage {
   save() {
     if (this.form.valid) {
       let entry = this.form.value;
-      Observable.combineLatest(this.scopeService.scope$, this.dateService.date$).first()
-        .subscribe(([scope, date]) => {
-          entry.scope = scope;
-          entry.start = date;
-          this.interviewService.addEntry(entry);
-          this.form.reset()
-        });
+      console.log(entry);
+      Observable.combineLatest(
+        this.scopeService.scope$,
+        this.dateService.date$,
+        (scope, date) => {
+          this.apollo.mutate({
+            mutation: AddInterviewEntryMutation,
+            variables: {
+              scope: scope.toUpperCase(),
+              start: getScopeStart(scope, date),
+              likes: entry.likes,
+              dislikes: entry.dislikes
+            }
+          }).subscribe(() => {
+            this.form.reset();
+            this.query$.refetch();
+          })
+        }
+      ).first().subscribe()
     }
   }
 
