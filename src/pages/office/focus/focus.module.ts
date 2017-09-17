855d0c1466daf844dbc60270d46d7a12d00ba312
Eraldo Energy <eraldo@eraldo.org>
2017-09-17 18:23:01 +0200

    Fixed focus title case.

--- src/pages/office/focus/focus.module.ts
+++ src/pages/office/focus/focus.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { FocusPage } from './focus';
 import {UserItemComponentModule} from "../../../components/user-item/user-item.module";
+import {OutcomeComponentModule} from "../../../components/outcome/outcome.module";
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import {UserItemComponentModule} from "../../../components/user-item/user-item.m
   imports: [
     IonicPageModule.forChild(FocusPage),
     UserItemComponentModule,
+    OutcomeComponentModule
   ],
 })
 export class FocusPageModule {}
