b35b264589aa6157786d4d74998d21ad88a327e0
Eraldo Energy <eraldo@eraldo.org>
2017-11-05 11:50:26 +0100

    Added news listing.

--- src/pages/project/news/news.ts
+++ src/pages/project/news/news.ts
@@ -1,5 +1,26 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Apollo} from "apollo-angular";
+import gql from "graphql-tag";
+
+const NewsQuery = gql`
+  query {
+    news: newsItems(first: 10) {
+      edges {
+        node {
+          id
+          name
+          author {
+            name
+          }
+          date
+          imageUrl
+          description
+        }
+      }
+    }
+  }
+`;
 
 @IonicPage()
 @Component({
@@ -7,8 +28,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'news.html',
 })
 export class NewsPage {
+  query$;
+  loading = true;
+  news;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo) {
+  }
+
+  ngOnInit() {
+    this.query$ = this.apollo.watchQuery({query: NewsQuery});
+    this.query$.subscribe(({data, loading}) => {
+      this.loading = loading;
+      this.news = data && data.news.edges;
+    })
   }
 
   ionViewDidLoad() {
