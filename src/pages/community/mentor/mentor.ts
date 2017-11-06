1e7453bc257fe7d3e3dac3f7624a21fd6f8d8398
Eraldo Energy <eraldo@eraldo.org>
2017-11-06 14:59:33 +0100

    Made mentor page dynamic showing the assigned mentor.

--- src/pages/community/mentor/mentor.ts
+++ src/pages/community/mentor/mentor.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import gql from "graphql-tag";
+import {Apollo} from "apollo-angular";
+
+const MentorQuery = gql`
+  query {
+    user: myUser {
+      id
+      mentor {
+        id
+        name
+      }
+    }
+  }
+`;
 
 @IonicPage()
 @Component({
@@ -7,8 +21,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'mentor.html',
 })
 export class MentorPage {
+  query$;
+  loading = true;
+  mentor;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo) {
+  }
+
+  ngOnInit() {
+    const id = this.navParams.get('id');
+    this.query$ = this.apollo.watchQuery({
+      query: MentorQuery,
+    });
+    this.query$.subscribe(({data, loading}) => {
+      this.loading = loading;
+      this.mentor = data && data.user.mentor;
+    })
   }
 
   ionViewDidLoad() {
