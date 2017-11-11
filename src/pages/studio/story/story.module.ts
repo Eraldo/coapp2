8e6593304592d9b2d9168418ce16572e2aacdca3
Eraldo Energy <eraldo@eraldo.org>
2017-11-11 19:40:09 +0100

    Added story chapters.

--- src/pages/studio/story/story.module.ts
+++ src/pages/studio/story/story.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { StoryPage } from './story';
 import {AppToolbarComponentModule} from "../../../components/app-toolbar/app-toolbar.module";
+import {MarkdownModule} from "angular2-markdown";
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import {AppToolbarComponentModule} from "../../../components/app-toolbar/app-too
   imports: [
     IonicPageModule.forChild(StoryPage),
     AppToolbarComponentModule,
+    MarkdownModule
   ],
   exports: [
     StoryPage
--- src/pages/studio/story/story.ts
+++ src/pages/studio/story/story.ts
@@ -1,20 +1,51 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import gql from "graphql-tag";
+import {Apollo} from "apollo-angular";
+import {MarkdownService} from "angular2-markdown";
 
-/**
- * Generated class for the StoryPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+const Query = gql`
+  query {
+    viewer {
+      story {
+        id
+        chapters(first: 1) {
+          edges {
+            node {
+              id
+              name
+              content
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 @IonicPage()
 @Component({
   selector: 'page-story',
   templateUrl: 'story.html',
 })
 export class StoryPage {
+  query$;
+  loading = true;
+  chapters;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo, private markdownService: MarkdownService) {
+    // Workaround: https://github.com/dimpu/angular2-markdown/issues/65
+    // this.markdownService.setMarkedOptions({gfm: true, breaks: true, sanitize: true});
+    this.markdownService.setMarkedOptions({gfm: true, breaks: true});
+  }
+
+  ngOnInit() {
+    this.query$ = this.apollo.watchQuery({query: Query});
+    this.query$.subscribe(({data, loading}) => {
+      this.loading = loading;
+      console.log(data);
+      this.chapters = data && data.viewer && data.viewer.story && data.viewer.story.chapters;
+    })
   }
 
   ionViewDidLoad() {
