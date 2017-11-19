4d1424422054cca3dc3aac7e1ee0ccd09e8ed04f
Eraldo Energy <eraldo@eraldo.org>
2017-11-19 21:03:44 +0100

    Added error message.

--- src/pages/settings/tags/tags.ts
+++ src/pages/settings/tags/tags.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
 import gql from "graphql-tag";
 import {Apollo} from "apollo-angular";
 
@@ -38,7 +38,7 @@ export class TagsPage {
   query$;
   tags;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo, private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo, private alertCtrl: AlertController, public toastCtrl: ToastController) {
   }
 
   ngOnInit() {
@@ -57,13 +57,14 @@ export class TagsPage {
     this.query$.refetch();
   }
 
-  create() {
+  create(name = '') {
     let prompt = this.alertCtrl.create({
       title: 'Name',
       inputs: [
         {
           name: 'name',
           placeholder: 'Name',
+          value: name
         },
       ],
       buttons: [
@@ -77,13 +78,22 @@ export class TagsPage {
           text: 'Save',
           handler: data => {
             const name = data.name;
+            if (name.length > 2) {
               this.apollo.mutate({
                 mutation: CreateMutation,
                 variables: {name, description: ''},
                 refetchQueries: [{query: Query}]
               })
+            } else {
+              let toast = this.toastCtrl.create({
+                message: 'Please enter a tag name with at least two characters.',
+                duration: 3000
+              });
+              toast.present();
+              this.create(name);
             }
           }
+        }
       ]
     });
     prompt.present();
