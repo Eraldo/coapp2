0be575e1472fc318f34436b565993a96fa030a66
Eraldo Energy <eraldo@eraldo.org>
2017-11-27 13:44:51 +0100

    Added stats page mockup.

--- src/pages/home/stats/stats.ts
+++ src/pages/home/stats/stats.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams, PopoverController} from 'ionic-angular';
 import {Apollo} from "apollo-angular";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ScopeService} from "../../../services/scope/scope";
 
 @IonicPage()
 @Component({
@@ -8,12 +10,39 @@ import {Apollo} from "apollo-angular";
   templateUrl: 'stats.html',
 })
 export class StatsPage {
+  loading = true;
+  query$;
+  private form: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo, public popoverCtrl: PopoverController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo, private formBuilder: FormBuilder, private scopeService: ScopeService, public popoverCtrl: PopoverController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StatsPage');
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      // scope: [scope.toUpperCase(), Validators.required],
+      // start: [],
+      area1: [50, Validators.required],
+      area2: [50, Validators.required],
+      area3: [50, Validators.required],
+      area4: [50, Validators.required],
+      area5: [50, Validators.required],
+      area6: [50, Validators.required],
+      area7: [50, Validators.required],
+    });
+  }
+
+  save() {
+    if (this.form.valid) {
+      console.log(this.form.value)
+    }
+  }
+
+  showTutorial(name) {
+    this.navCtrl.push('TutorialPage', {name})
   }
 
   showOptions(source) {
