483d0c4f4ed5a531ee5aba5aac77b74b5121d952
Eraldo Energy <eraldo@eraldo.org>
2017-12-14 20:58:12 +0100

    Marked typewriter as safe after cleaning.

--- src/components/typewriter/typewriter.module.ts
+++ src/components/typewriter/typewriter.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import {TypewriterComponent} from "./typewriter";
+import {PipesModule} from "../../pipes/pipes.module";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import {TypewriterComponent} from "./typewriter";
   ],
   imports: [
     IonicPageModule.forChild(TypewriterComponent),
+    PipesModule
   ],
   exports: [
     TypewriterComponent
