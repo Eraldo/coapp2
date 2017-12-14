694dfd69a265a32efc9736f11a7aa22f1c184299
Eraldo Energy <eraldo@eraldo.org>
2017-12-14 02:30:07 +0100

    Added star rating input.

--- src/pages/arcade/adventures/adventure/adventure-review-form/adventure-review-form.module.ts
+++ src/pages/arcade/adventures/adventure/adventure-review-form/adventure-review-form.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AdventureReviewFormPage } from './adventure-review-form';
 import {SimplemdeModule} from "ng2-simplemde/no-style";
+import {Ionic2RatingModule} from "ionic2-rating";
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import {SimplemdeModule} from "ng2-simplemde/no-style";
   ],
   imports: [
     IonicPageModule.forChild(AdventureReviewFormPage),
-    SimplemdeModule
+    SimplemdeModule,
+    Ionic2RatingModule
   ],
 })
 export class AdventureReviewFormPageModule {}
