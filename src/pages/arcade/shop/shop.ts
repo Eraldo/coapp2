69f29bdf61d04ebf45e060e1c3cf2a41c1452881
Eraldo Energy <eraldo@eraldo.org>
2018-03-16 21:03:50 +0100

    Added balance to shop.

--- src/pages/arcade/shop/shop.ts
+++ src/pages/arcade/shop/shop.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import {IonicPage, NavController, NavParams, PopoverController} from 'ionic-angular';
 import {Apollo} from "apollo-angular";
+import gql from "graphql-tag";
+
+const ViewerQuery = gql`
+  query {
+    user: viewer {
+      id
+      balance
+    }
+  }
+`;
 
 @IonicPage()
 @Component({
@@ -8,8 +18,21 @@ import {Apollo} from "apollo-angular";
   templateUrl: 'shop.html',
 })
 export class ShopPage {
+  loading = true;
+  query$;
+  balance;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo, public popoverCtrl: PopoverController) {
+  }
+
+  ngOnInit() {
+    this.query$ = this.apollo.watchQuery({
+      query: ViewerQuery,
+    });
+    this.query$.subscribe(({data, loading}) => {
+      this.loading = loading;
+      this.balance = data && data.user && data.user.balance;
+    })
   }
 
   ionViewDidLoad() {
