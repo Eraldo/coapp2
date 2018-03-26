a69a752ec513c07be770d963ef881daef82d813c
Eraldo Energy <eraldo@eraldo.org>
2018-03-26 04:50:29 +0200

    Added full-screen toggle to text modal.

--- src/pages/lib/text-modal/text-modal.ts
+++ src/pages/lib/text-modal/text-modal.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Renderer, ViewChild} from '@angular/core';
 import {IonicPage, NavParams, ViewController} from 'ionic-angular';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Hotkey, HotkeysService} from "angular2-hotkeys";
@@ -9,12 +9,15 @@ import {Hotkey, HotkeysService} from "angular2-hotkeys";
   templateUrl: 'text-modal.html',
 })
 export class TextModalPage {
+  @ViewChild('editor') editor;
   private form: FormGroup;
   title: string;
   content: string;
   required: boolean;
+  fullScreen = false;
+  options = {};  // https://github.com/sparksuite/simplemde-markdown-editor
 
-  constructor(public viewCtrl: ViewController, public navParams: NavParams, private formBuilder: FormBuilder, private hotkeysService: HotkeysService) {
+  constructor(public viewCtrl: ViewController, public navParams: NavParams, private formBuilder: FormBuilder, private hotkeysService: HotkeysService, public renderer: Renderer) {
     this.title = this.navParams.get('title') || 'Text Editor';
     this.content = this.navParams.get('content') || '';
     this.required = this.navParams.get('required') || false;
@@ -35,6 +38,12 @@ export class TextModalPage {
 
   cancel() {
     this.viewCtrl.dismiss();
+  }
+
+  toggleFullScreen() {
+    this.fullScreen = !this.fullScreen;
+    this.editor.simplemde.toggleFullScreen();
+    this.renderer.setElementClass(this.viewCtrl.pageRef().nativeElement, 'modal-fullscreen', this.fullScreen);
   }
 
   ionViewDidLoad() {
