6babc8d3bd2fa27ac98e9f778220d0ee2f4bf629
Eraldo Energy <eraldo@eraldo.org>
2018-04-03 14:48:08 +0200

    Replaced scoped date display.

--- src/pages/office/agenda/focus-form/focus-form.module.ts
+++ src/pages/office/agenda/focus-form/focus-form.module.ts
@@ -3,6 +3,7 @@ import { IonicPageModule } from 'ionic-angular';
 import { FocusFormPage } from './focus-form';
 import {OutcomeInputComponentModule} from "../../../../components/outcome-input/outcome-input.module";
 import {ElasticModule} from "angular2-elastic";
+import {ScopedDateComponentModule} from "../../../../components/scoped-date/scoped-date.module";
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import {ElasticModule} from "angular2-elastic";
   imports: [
     IonicPageModule.forChild(FocusFormPage),
     OutcomeInputComponentModule,
+    ScopedDateComponentModule,
     ElasticModule
   ],
   exports: [
