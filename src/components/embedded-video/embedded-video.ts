6887201ac15e1def6ecf7b42dd3c57f523a512a4
Eraldo Energy <eraldo@eraldo.org>
2018-04-19 20:18:53 +0200

    Added default query options to embedded video player.

--- src/components/embedded-video/embedded-video.ts
+++ src/components/embedded-video/embedded-video.ts
@@ -12,16 +12,49 @@ export class EmbeddedVideoComponent {
   width;
   @Input()
   height;
+  @Input()
+  query;
+
+  // YT;
+  // player;
 
   get content() {
-    const attributes = {};
+    const attributes = {id: 'player'};
     if (this.width) attributes['width'] = this.width;
     if (this.height) attributes['height'] = this.height;
-    return this.embedVideoService.embed(this.videoUrl, {attr: attributes})
+
+    // youtube options: velopers.google.com/youtube/player_parameters
+    let query = {rel: 0, modestbranding: 1, origin: window.location.origin, enablejsapi: 1};
+    if (this.query) Object.assign(query, this.query);
+
+    return this.embedVideoService.embed(this.videoUrl, {attr: attributes, query})
   }
 
   constructor(private embedVideoService: EmbedVideoService) {
     console.log('Hello EmbeddedVideoComponent Component');
   }
 
+  // ngOnInit() {
+  //   var tag = document.createElement('script');
+  //   tag.src = 'https://www.youtube.com/iframe_api';
+  //   var firstScriptTag = document.getElementsByTagName('script')[0];
+  //   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  //
+  //   window['onYouTubeIframeAPIReady'] = (e) => {
+  //     this.YT = window['YT'];
+  //     this.player = new window['YT'].Player('player', {
+  //       events: {
+  //         // 'onStateChange': this.onPlayerStateChange.bind(this),
+  //         // 'onError': this.onPlayerError.bind(this),
+  //         // 'onReady': (e) => {
+  //         //   alert('ready');
+  //         //   this.play();
+  //         // }
+  //       }
+  //     });
+  //   };
+  // }
+  // play() {
+  //   this.player.playVideo()
+  // }
 }
