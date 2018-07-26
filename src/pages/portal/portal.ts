910a6e3d9bf63a7da9277993f05fa02859d432a3
Eraldo Energy <eraldo@eraldo.org>
2018-07-26 13:00:37 +0200

    Added custom app portals feature.

---
+++ src/pages/portal/portal.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import {Icon} from "../../models/icon";
+import {Apollo} from "apollo-angular";
+import gql from "graphql-tag";
+
+const EnableChatMutation = gql`
+  mutation EnableChat {
+    enableChat(input: {}) {
+      success
+    }
+  }
+`;
+
+@IonicPage({
+  segment: 'portal/:id'
+})
+@Component({
+  selector: 'page-portal',
+  templateUrl: 'portal.html',
+})
+export class PortalPage {
+  icons;
+  message = 'Entering portal...';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private apollo: Apollo, public toastCtrl: ToastController) {
+    this.icons = Icon;
+  }
+
+  ngOnInit() {
+    const id = this.navParams.get('id');
+    switch (id) {
+      case 'unlock-chat': {
+        this.apollo.mutate({
+          mutation: EnableChatMutation,
+        }).subscribe(() => {
+          let toast = this.toastCtrl.create({
+            message: "I successfully joined the chat.",
+            duration: 2000,
+            cssClass: 'success'
+          });
+          toast.present();
+          this.message = 'Entered chat portal.';
+          this.navCtrl.push('QuestPage');
+        });
+        break;
+      }
+    }
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PortalPage');
+  }
+
+}
